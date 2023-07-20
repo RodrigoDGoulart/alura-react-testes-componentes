7158de8998d72856ff04f41c48815fc29a6fe6c8
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-07-19 21:43:54 -0300

    feat: add react-router-dom and recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import Formulario from "./componentes/Formulario/Formulario";
+
 export default function App() {
   return (
-    <h1>Olá</h1>
+    <BrowserRouter>
+      <RecoilRoot>
+        <Routes>
+          <Route path='/' element={Formulario}/>
+        </Routes>
+      </RecoilRoot>
+    </BrowserRouter>
   );
 }
