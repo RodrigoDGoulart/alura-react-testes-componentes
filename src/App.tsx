31f17fd538b0f2939e8e463853d1cb7745e4edb0
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-07-28 18:44:39 -0300

    feat: add css style in Sorteio component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import Configuracao from "./paginas/Configuracao/Configuracao";
+import Sorteio from "./paginas/Sorteio/Sorteio";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <RecoilRoot>
         <Routes>
           <Route path='/' element={<Configuracao />} />
+          <Route path='/sorteio' element={<Sorteio />} />
         </Routes>
       </RecoilRoot>
     </BrowserRouter>
--- src/paginas/Sorteio/Sorteio.tsx
+++ src/paginas/Sorteio/Sorteio.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { useListaParticipantes } from "../../state/hook/useListaParticipantes";
 import { useResultadoSorteio } from "../../state/hook/useResultadoSorteio";
 
+import './Sorteio.css';
+import Card from "../../componentes/Card";
+
 export default function Sorteio() {
 
   const participantes = useListaParticipantes();
@@ -14,28 +17,36 @@ export default function Sorteio() {
   const sortear = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (resultado.has(participanteDaVez)) {
-      setAmigoSecreto(resultado.get(participanteDaVez)!); 
+      setAmigoSecreto(resultado.get(participanteDaVez)!);
     }
   }
 
   return (
-    <section>
-      <form onSubmit={sortear}>
-        <select
-          required
-          name='participanteDavez'
-          id="participanteDavez"
-          value={participanteDaVez}
-          placeholder="Selecione o seu nome"
-          onChange={e => setParticipanteDaVez(e.target.value)}
-        >
-          {participantes.map(participante => (
-            <option key={participante}>{participante}</option>
-          ))}
-        </select>
-        <button>Sortear</button>
-      </form>
-      {amigoSecreto && <p role='alert'>{amigoSecreto}</p>}
-    </section>
+    <Card>
+
+      <section className="sorteio">
+        <h2>Quem vai tirar o papelzinho?</h2>
+        <form onSubmit={sortear}>
+          <select
+            required
+            name='participanteDavez'
+            id="participanteDavez"
+            value={participanteDaVez}
+            placeholder="Selecione o seu nome"
+            onChange={e => setParticipanteDaVez(e.target.value)}
+          >
+            {participantes.map(participante => (
+              <option key={participante}>{participante}</option>
+            ))}
+          </select>
+          <p>Clique em sortear para ver quem é seu amigo secreto!</p>
+          <button className="botao-sortear">Sortear</button>
+        </form>
+        {amigoSecreto && <p className="resultado" role='alert'>{amigoSecreto}</p>}
+        <footer className="sorteio">
+          <img src="/imagens/aviao.png" className="aviao" alt="Um desenho de um avião de papel" />
+        </footer>
+      </section>
+    </Card>
   );
 }
