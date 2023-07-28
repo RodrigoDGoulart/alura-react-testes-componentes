e8d637467797670e12cfa5c2a79731372d2ee452
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-07-28 18:18:34 -0300

    test: useSorteador needs to be called by Rodape component

--- src/componentes/Rodape/Rodape.tsx
+++ src/componentes/Rodape/Rodape.tsx
@@ -2,15 +2,19 @@ import { useNavigate } from "react-router-dom";
 import { useListaParticipantes } from "../../state/hook/useListaParticipantes";
 
 import './Rodape.css';
+import { useSorteador } from "../../state/hook/useSorteador";
 
 export default function Rodape () {
   const participantes = useListaParticipantes();
 
   const navegarPara = useNavigate();
+  const sortear = useSorteador();
 
   const iniciar = () => {
+    sortear(); 
     navegarPara('/sorteio');
   }
+
 
   return(
     <footer className="rodape-configuracoes">
